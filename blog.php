8faba48c5be9e8c0c65ac6e2633bdc6174002a42
Sair Garces <sgrivas.marual@gmail.com>
2023-10-28 15:44:34 -0500

    three commit

--- blog.php
+++ blog.php
@@ -1,11 +1,51 @@
 <?php
- include 'header.php'
+include 'header.php'
 ?>
 
-    <main class="contenedor seccion">
-        <h1>Titulo Página blog y tips</h1>
-    </main>
+<main class="contenedor seccion">
+    <h1>Titulo Página blog y tips</h1>
+</main>
+<section class="contenedor">
+    <div class="cards_blog">
+        <div class="card_tips">
+            <img src="assets/img/15.jpg" alt="">
+            <div class="textos-cards">
+                <h3>¿Cómo comprar una casa?</h3>
+                <div class="textos_card">
+                    <h4>1. Investiga Antes de Comprar</h4>
+                    <p>Antes de comprar tu primera vivienda, investiga el mercado, compara precios, conoce tus necesidades y establece un presupuesto realista. La inversión en tiempo y esfuerzo en la investigación puede ayudarte a tomar decisiones informadas.</p>
+                </div>
+                <div class="textos_card">
+                    <h4>2. La Importancia de la Inspección de la Vivienda</h4>
+                    <p>No subestimes la importancia de una inspección de la vivienda. Contratar a un inspector profesional puede revelar problemas ocultos en la propiedad que podrían ser costosos de reparar más adelante. Esto te ayudará a tomar decisiones más seguras.</p>
+                </div>
+                <div class="textos_card">
+                    <h4>3. Mantén un Ojo en los Costos Ocultos</h4>
+                    <p>Además del precio de compra, ten en cuenta los costos de cierre, impuestos, seguros y costos de mantenimiento. No te dejes sorprender por gastos inesperados. Planifica tu presupuesto cuidadosamente.</p>
+                </div>
+            </div>
+        </div>
+        <div class="card_tips">
+            <img src="assets/img/15.jpg" alt="">
+            <div class="textos-cards">
+                <h3>¿Cómo comprar una casa?</h3>
+                <div class="textos_card">
+                    <h4>1. Investiga Antes de Comprar</h4>
+                    <p>Antes de comprar tu primera vivienda, investiga el mercado, compara precios, conoce tus necesidades y establece un presupuesto realista. La inversión en tiempo y esfuerzo en la investigación puede ayudarte a tomar decisiones informadas.</p>
+                </div>
+                <div class="textos_card">
+                    <h4>2. La Importancia de la Inspección de la Vivienda</h4>
+                    <p>No subestimes la importancia de una inspección de la vivienda. Contratar a un inspector profesional puede revelar problemas ocultos en la propiedad que podrían ser costosos de reparar más adelante. Esto te ayudará a tomar decisiones más seguras.</p>
+                </div>
+                <div class="textos_card">
+                    <h4>3. Mantén un Ojo en los Costos Ocultos</h4>
+                    <p>Además del precio de compra, ten en cuenta los costos de cierre, impuestos, seguros y costos de mantenimiento. No te dejes sorprender por gastos inesperados. Planifica tu presupuesto cuidadosamente.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
- include 'footer.php'
+include 'footer.php'
 ?>

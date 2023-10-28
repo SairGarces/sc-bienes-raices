19c898a28588f295b910ece5ba4a161031664a58
Sair Garces <sgrivas.marual@gmail.com>
2023-10-28 02:07:28 -0500

    second commit

---
+++ blog.php
@@ -0,0 +1,11 @@
+<?php
+ include 'header.php'
+?>
+
+    <main class="contenedor seccion">
+        <h1>Titulo Página blog y tips</h1>
+    </main>
+
+<?php
+ include 'footer.php'
+?>
---
+++ nosotros.php
@@ -0,0 +1,56 @@
+<?php
+include 'header.php'
+?>
+
+<main class="contenedor seccion">
+    <h1>Sobre Nosotros</h1>
+</main>
+<div class="contenedor">
+    <h3 class="Sub_tittle">Quiénes Somos</h3>
+    <div class="nosotros_1">
+        <div class="texto_nosotros">
+            <p>
+                En SC Bienes Raíces, nos enorgullece ser tu socio confiable en el emocionante mundo de los bienes raíces. Somos un equipo apasionado de profesionales dedicados a ayudarte a alcanzar tus objetivos inmobiliarios. Con años de experiencia en el mercado, hemos construido una reputación sólida y una extensa red de contactos en la industria. Nos esforzamos por brindarte el mejor servicio y asesoramiento en bienes raíces, ya sea que estés buscando comprar, vender o invertir en propiedades.
+            </p>
+        </div>
+        <div class="image_logo">
+            <img src="assets/icons/logo.png" alt="">
+        </div>
+    </div>
+    <div class="mision">
+        <h3 class="Sub_tittle2">Nuestra Misión</h3>
+        <div class="nosotros_1">
+            <div class="Image_mision">
+                <img src="assets/icons/mision.png" alt="">
+            </div>
+            <div class="texto_mision">
+                <p>
+                    Nuestra misión es simple pero poderosa: Facilitar tus sueños inmobiliarios. Ya sea que desees encontrar la casa de tus sueños, vender tu propiedad al mejor precio o explorar oportunidades de inversión, estamos aquí para hacer que tu viaje inmobiliario sea exitoso y sin complicaciones
+                </p>
+            </div>
+        </div>
+    </div>
+    <section class="entradas_index">
+        <div class="entradas_indexs">
+            <div class="firs_entra">
+                <img src="assets/icons/comercio-justo.png" alt="">
+                <h3>Compromiso</h3>
+                <p>"Nuestro compromiso es tu tranquilidad. En SC Bienes Raíces, nos dedicamos en cuerpo y alma a cuidar tus sueños inmobiliarios, trabajando incansablemente para que cada paso sea seguro y satisfactorio."</p>
+            </div>
+            <div class="secund_entra">
+                <img src="assets/icons/innovacion.png" alt="">
+                <h3>Innovación</h3>
+                <p>"La innovación es nuestro motor. En SC Bienes Raíces, adoptamos las últimas tecnologías y enfoques creativos para hacerte la vida más fácil en el mundo inmobiliario. Más eficiencia, más comodidad, más futuro."</p>
+            </div>
+            <div class="tird_entra">
+                <img src="assets/icons/calidad.png" alt="">
+                <h3>Calidad</h3>
+                <p>"La calidad es nuestra firma. En SC Bienes Raíces, nos comprometemos con la excelencia. Cada propiedad, cada transacción, y cada relación está impregnada de la más alta calidad, porque mereces lo mejor."</p>
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php
+include 'footer.php'
+?>
